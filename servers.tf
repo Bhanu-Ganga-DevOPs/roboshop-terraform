f78fbb08f11361e2f4b78f25a3867779f726ca0b
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-05-13 15:56:34 -0400

    testing

---
+++ servers.tf
@@ -0,0 +1,141 @@
+data "aws_ami" "centos" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+output "image_id" {
+   value = data.aws_ami.centos.image_id
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+
+# Terraform aws_instance creation
+
+resource "aws_instance" "dispatch" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "dispatch"
+  }
+}
