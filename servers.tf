feb5649029507c1b98df702ab61657d488451adf
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-05-20 16:27:06 -0400

    testing

--- servers.tf
+++ servers.tf
@@ -14,22 +14,96 @@ variable "instance_type" {
 
 
 variable "components" {
-  default = ["frontend","user","catalogue","cart","shipping","payment","mongodb","mysql","redis","rabbitmq","dispatch"]
+  # default = ["frontend","user","catalogue","cart","shipping","payment","mongodb","mysql","redis","rabbitmq","dispatch"]
+  default = {
+    "frontend" = {
+      name          = "frontend"
+      instance_type = "t3.small"
+    },
+    "user" = {
+      name          = "user"
+      instance_type = "t3.micro"
+    },
+    "catalogue" = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    },
+    "cart" = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    },
+    "shipping" = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+    },
+    "payment" = {
+      name          = "payment"
+      instance_type = "t3.small"
+    },
+    "mongodb" = {
+      name          = "mongodb"
+      instance_type = "t3.small"
+    },
+    "mysql" = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    },
+    "redis" = {
+      name          = "redis"
+      instance_type = "t3.small"
+    },
+    "rabbitmq" = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    },
+    "dispatch" = {
+      name          = "dispatch"
+      instance_type = "t3.small"
+    }
+  }
 }
 
 
 
 resource "aws_instance" "instance" {
-  count = length(var.components)
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+
+  ################## resource creation using Count #################
+#  count = length(var.components)
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [data.aws_security_group.allow-all-security-group.id]
+#
+#  tags = {
+#    Name = var.components[count.index]
+#  }
+  ################## END of resource creation using Count #################
+
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all-security-group.id]
 
   tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
 }
-#
+
+
+resource "aws_route53_record" "dns-record" {
+  for_each               = var.components
+  zone_id                = "Z02296781GEG0AJVP8QEV"
+  name                   = "${each.value["name"]}-dev.gangabhavanikatraparthi.online"
+  type                   = "A"
+  ttl                    = 30
+  records                = [aws_instance.instance[each.value["name"]].private_ip]
+}
+
+
+
+
+
+################## Manual creation of resource and Route53 records creation using Count #################
+
 #resource "aws_instance" "user" {
 #  ami           = data.aws_ami.centos.image_id
 #  instance_type = var.instance_type
@@ -130,14 +204,7 @@ resource "aws_instance" "instance" {
 #  }
 #}
 
-resource "aws_route53_record" "dns-record" {
-  count   = length(var.components)
-  zone_id = "Z02296781GEG0AJVP8QEV"
-  name    = "${var.components[count.index]}-dev.gangabhavanikatraparthi.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance.private_ip]
-}
+
 #
 #resource "aws_route53_record" "user" {
 #  zone_id = "Z02296781GEG0AJVP8QEV"
@@ -218,3 +285,6 @@ resource "aws_route53_record" "dns-record" {
 #  ttl     = 30
 #  records = [aws_instance.dispatch.private_ip]
 #}
+
+
+################## End of Manual creation of resource and Route53 records creation using Count #################
