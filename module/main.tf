8207cd23ec1066cb2069d3e35c34779677bab987
Bhanu-Ganga-DevOPs <gangabhavanikatraprthi@gmail.com>
2023-06-05 14:45:33 -0400

    testing

--- module/main.tf
+++ module/main.tf
@@ -1,4 +1,4 @@
-resource  "aws_instance" "instance"{
+resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all-security-group.id]
@@ -10,7 +10,7 @@ resource  "aws_instance" "instance"{
 
 resource "null_resource" "provisioner" {
 
-  depends_on = [aws_instance.instance, aws_route53_record.dns-record]
+  depends_on = [aws_instance.instance, aws_route53_record.records]
   provisioner "remote-exec" {
 
     connection {
@@ -21,20 +21,19 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "echo -------- ${var.component_name} ---------",
       "rm -rf roboshop-shell",
       "git clone https://github.com/Bhanu-Ganga-DevOPs/roboshop-shell",
       "cd roboshop-shell",
-      "sudo bash ${var.component_name}.sh  ${lookup(var.password,"password","null")} "
+      "sudo bash ${var.component_name}.sh ${var.password}"
     ]
   }
 }
 
-resource "aws_route53_record" "dns-record" {
 
-  zone_id                = "Z02296781GEG0AJVP8QEV"
-  name                   = "${var.component_name}-dev.gangabhavanikatraparthi.online"
-  type                   = "A"
-  ttl                    = 30
-  records                = [aws_instance.instance.private_ip]
+resource "aws_route53_record" "records" {
+  zone_id = "Z02296781GEG0AJVP8QEV"
+  name    = "${var.component_name}-dev.gangabhavanikatraparthi.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
 }
--- module/vars.tf
+++ module/vars.tf
@@ -1,13 +1,5 @@
-
-variable "components" {
-  # default = ["frontend","user","catalogue","cart","shipping","payment","mongodb","mysql","redis","rabbitmq","dispatch"]
-}
-
-variable "env" {}
-
 variable "instance_type" {}
-
 variable "component_name" {}
-
 variable "password" {}
+
 
